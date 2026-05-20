620e05390888a877f71b07c92169a46b102b41c1
Jyotsana Katare <jyotsanakatare63@gmail.com>
2026-05-20 19:15:59 +0530

    updated

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -7,9 +7,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
+const isProduction = import.meta.env.PROD;
+
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
+    <BrowserRouter basename={isProduction ? "/EasyShopAdmin" : "/"}>
       <App />
     </BrowserRouter>
   </QueryClientProvider>
