ffe3f4c2b4419758ea08af44b88f2c2726d55fff
Jyotsana Katare <jyotsanakatare63@gmail.com>
2026-07-15 11:04:17 +0530

    added frontend url

--- API/utils/sendEmail.js
+++ API/utils/sendEmail.js
@@ -10,7 +10,7 @@ const sendEmail = async (to, subject, html) => {
     try {
         const { data, error } = await resend.emails.send({
             from: 'EasyShop Support <onboarding@resend.dev>',
-            to: [to],
+            to: [process.env.EMAIL_USER],
             subject: subject,
             html: html,
         });
